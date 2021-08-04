c63e05e64e3675e8b519ca34ed91ba46e3d5d5f8
Giorgi Kalandadze <gkalandadze@bog.ge>
2021-08-04 23:49:41 +0400

    Add Button added

--- FinalProject/app/src/main/java/ge/dgoginashvili_gkalandadze/finalproject/MainActivity.kt
+++ FinalProject/app/src/main/java/ge/dgoginashvili_gkalandadze/finalproject/MainActivity.kt
@@ -4,10 +4,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ge.dgoginashvili_gkalandadze.finalproject.activities.LoginActivity
+import ge.dgoginashvili_gkalandadze.finalproject.activities.MainFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    private fun initBottomMenu() {
+        supportFragmentManager.beginTransaction().replace(R.id.main_frame, MainFragment()).commit()
+    }
+
 }
---
+++ FinalProject/app/src/main/java/ge/dgoginashvili_gkalandadze/finalproject/activities/MainFragment.kt
@@ -0,0 +1,6 @@
+package ge.dgoginashvili_gkalandadze.finalproject.activities
+
+import androidx.fragment.app.Fragment
+
+class MainFragment : Fragment(){
+}
